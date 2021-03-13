cfbac52191bb27e6470d7e08be7602054d53cc40
Bharatendu Soumil <you@example.com>
2021-03-13 03:49:44 +0100

    add input to findInteractive func and change name of that func

--- driver_utils.py
+++ driver_utils.py
@@ -2,4 +2,4 @@ import utils
 
 if __name__ == '__main__':
   
-    utils.showInteractive('newplot.pickle')     
+    utils.showInteractivePlot()     
--- utils.py
+++ utils.py
@@ -7,12 +7,16 @@ Created on 12.03.2021
 import pickle
 import matplotlib.pyplot as plt
 import utils
-def showInteractive(file): #show graph as matplotlib interactive object
-    
-     fig = pickle.load(open(file, 'rb'))
+def showInteractivePlot(): #show graph as matplotlib interactive object
+     file=input('Enter your filename:') 
+     try:
+         fig = pickle.load(open(file, 'rb'))
      #see here for more info https://www.david.science/py-pickle-plot.html
 #     ax.set_ylabel('Voltage (mV)')
 #     ax.minorticks_on()
 #     ax.legend()
-     plt.show()
-     
+         plt.show()
+     except:
+         print("No such file found...Exitting!")
+         return
+    
